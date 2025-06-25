32885aa1d01b17eaf3603ed6246b692660199caf
Roland <rolandosalajr@gmail.com>
2025-06-25 20:49:51 +0800

    testing

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
-import Home from '../components/Home.vue'
 const routes = [
-    { path: '/', component: Home },
-    { path: '/about', component: import ('../components/About.vue')}
+    { path: '/', component: () => import('../components/Home.vue') },
+    { path: '/about', component: () => import('../components/About.vue') }
 ]
 
 const router = createRouter({
