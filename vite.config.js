0b1d1cef69f93bf318e842adb4a63898ce8e21cf
Roland <rolandosalajr@gmail.com>
2025-06-25 21:21:04 +0800

    testing

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,16 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    viteStaticCopy({
+      targets: [
+        { src: 'dist/index.html', dest: '', rename: '404.html' }
+      ]
+    })
+  ],
   base: '/sample_2'
 })
